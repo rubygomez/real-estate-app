dfc5f637633462f9080674287d554418d0cbc594
rubygomez <rvbii.gomez@gmail.com>
2024-03-01 22:41:42 -0800

    created Home attributes, jbuilder, and controller

---
+++ db/migrate/20240302063935_create_homes.rb
@@ -0,0 +1,17 @@
+class CreateHomes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :homes do |t|
+      t.text :description
+      t.integer :year_built
+      t.integer :square_feet
+      t.decimal :bedrooms
+      t.decimal :bathrooms
+      t.integer :floors
+      t.string :availability
+      t.string :address
+      t.decimal :price
+
+      t.timestamps
+    end
+  end
+end
